82e5c44abe6439c745bc0d39f5aeaca34c9f9fca
tejesh <tejeshmuralikrishna@gmail.com>
2020-10-21 12:57:07 +0530

    edit

--- src/firebase/firebaseIndex.js
+++ src/firebase/firebaseIndex.js
@@ -2,11 +2,16 @@ import firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/app';
 
+// const admin = require('firebase-admin');
+// const projectId = admin.instanceId().app.options.projectId
+
+
 var firebaseConfig = {
 	apiKey: process.env.REACT_APP_API_KEY,
 	authDomain: process.env.REACT_APP_AUTHDOMAIN,
 	databaseURL: process.env.REACT_APP_BASEURL,
-	projectId: process.env.REACT_APP_PROJECT_ID,
+	// projectId: process.env.REACT_APP_PROJECT_ID,
+	projectId: "ecommerce-firebase-8f5f8",
 	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
 	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 	appId: process.env.REACT_APP_APP_ID,
